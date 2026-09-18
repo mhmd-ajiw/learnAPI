8522f715c4f226f1e7ef91bc80390b6623511491
mhmd-ajiw <journeyofaji@gmail.com>
2026-09-18 19:40:33 +0700

    exercise 1 async/await

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// Soal 2 (Chaining)
-function ambilBuah() {
-  return new Promise(function (resolve) {
-    setTimeout(function () { resolve("Apel"); }, 1000);
+//soal 1 (Basic)
+function cekStok(barang) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (barang === "sold-out") {
+        reject("Stok habis");
+      } else {
+        resolve(`Stok ${barang} tersedia`);
+      }
+    }, 1000);
   });
 }
 
-function masakBuah(buah) {
-  return new Promise(function (resolve) {
-    setTimeout(function () { resolve(buah + " Jus"); }, 1000);
-  });
+async function pesanBarang() {
+  try {
+    const pesan = await cekStok("baju");
+    console.log(pesan);
+  } catch(err) {
+    console.error(err);
+  }
 }
 
-ambilBuah()
-  .then((take) => {
-    return masakBuah(take);
-  })
-  .then((buah) => {
-    console.log(buah);
-  })
-  .catch((error) => {
-    console.log("Gagal mengambil buah.");
-  });
+pesanBarang()
