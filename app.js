cf2e7e9fcb2bc24adab96678cad3ac5b7e23cceb
mhmd-ajiw <journeyofaji@gmail.com>
2026-09-18 15:27:56 +0700

    refresh

--- app.js
+++
@@ -1,9 +0,0 @@
-function ambilSuhu(kota, callback){
-    setTimeout( function(){
-        callback(suhu);
-    }, 1500);
-}
-
-ambilSuhu('Semarang', function(32, city) {
-    console.log(`suhu di ${city} : ${suhu}°C`);
-});
